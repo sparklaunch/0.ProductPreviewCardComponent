0bfd66dfccae401f09de07f2a6c3adbd023994e7
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-23 07:11:35 +0900

    Initial Commit

---
+++ ProductPreviewCardComponent/ProductPreviewCardComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductPreviewCardComponentApp.swift
+//  ProductPreviewCardComponent
+//
+//  Created by Jinwook Kim on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ProductPreviewCardComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
